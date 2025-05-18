979fb28217e54b6fb9015b39767c52de7a01be26
maegiko <kennethk.dev@gmail.com>
2025-05-18 20:10:08 +1000

    Added animations

--- static/script.js
+++ static/script.js
@@ -21,24 +21,6 @@ function checkHeight() {
   }
 }
 
-window.onscroll = () => {
-  sections.forEach((sec) => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute("id");
-
-    if (top >= offset && top < offset + height) {
-      navLinks.forEach((links) => {
-        links.classList.remove("active");
-        document
-          .querySelector("header nav ul li a[href*=" + id + " ]")
-          .classList.add("active");
-      });
-    }
-  });
-};
-
 menuIcon.onclick = () => {
   menuIcon.classList.toggle("bx-x");
   navbar.classList.toggle("active");
@@ -81,3 +63,23 @@ const skillObserver = new IntersectionObserver(
 );
 
 animatedSkills.forEach((el) => skillObserver.observe(el));
+
+const contentObserver = new IntersectionObserver(
+  (entries, obs) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animate");
+        obs.unobserve(entry.target); // Only trigger once
+      }
+    });
+  },
+  {
+    threshold: 0.3,
+  }
+);
+
+const content = document.querySelectorAll(
+  ".heading, .bar, .timeline, .projects-box, .contact-form, .social, .list, .copyright, .hey-there, .home-img, .home-second, .header"
+);
+
+content.forEach((el) => contentObserver.observe(el));
